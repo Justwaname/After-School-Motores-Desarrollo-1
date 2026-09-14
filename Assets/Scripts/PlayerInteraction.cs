509511ae33641f2bbc8a216e5eeca23c587a2e23
paput-78 <cuadradosimon290@gmail.com>
2026-09-14 11:41:38 -0300

    poder ganar al recolectar los objetos y mensaje en pantalla

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,3 +1,5 @@
+using TMPro;
+using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.InputSystem; 
 
@@ -8,6 +10,10 @@ public class PlayerInteraction : MonoBehaviour
     public InputAction interactAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/e");
 
     private InteractableObject objectToInteract; // Guarda el item con el que podemos interactuar
+    [SerializeField] TextMeshProUGUI text;
+
+    [SerializeField] int itemsRecolected;
+    [SerializeField] int itemsToEscape;
 
     // En el New Input System, las acciones deben habilitarse y deshabilitarse
     private void OnEnable()
@@ -25,6 +31,7 @@ public class PlayerInteraction : MonoBehaviour
         if (interactAction.WasPressedThisFrame() && objectToInteract != null)
         {
             objectToInteract.Interact();
+            itemsRecolected++;
         }
     }
 
@@ -35,6 +42,16 @@ public class PlayerInteraction : MonoBehaviour
         if (other.CompareTag("Interactable"))
         {
             objectToInteract = other.GetComponent<InteractableObject>();
+            text.text = other.name + "[E] to equip";
+        }
+        if (other.CompareTag("Door") && itemsRecolected == itemsToEscape) //si te acercás a la puerta y las tenes todas
+        {
+            text.text = "[E] escape";
+            if (Input.GetKeyDown(KeyCode.E)) { SceneChange(""); } //pasar valor de escena de victoria, ahora no pq no la tengo juas
+        }
+        if (other.CompareTag("Door") && itemsRecolected != itemsToEscape) //si te acercás a la puerta y no las tenés todas :(
+        {
+            text.text = "you can't escape yet";
         }
     }
 
@@ -46,5 +63,10 @@ public class PlayerInteraction : MonoBehaviour
             // se vacia la variable porque ya no estamos cerca
             objectToInteract = null;
         }
+        text.text = "";
+    }
+    private void SceneChange(string scene)
+    {
+
     }
 }
