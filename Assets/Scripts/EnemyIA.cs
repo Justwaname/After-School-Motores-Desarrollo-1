fd9c97cde2698eb35b457c0fadc7a06d0677d857
Al3jo01 <92282422+Al3jo01@users.noreply.github.com>
2026-09-16 00:15:10 -0300

    agrego modelo  y prefab del enemigo
    puse las animaciones y ademas adapte el script de "EnemyIA" para que funcione correctamente

--- Assets/Scripts/EnemyIA.cs
+++ Assets/Scripts/EnemyIA.cs
@@ -4,7 +4,7 @@ using Unity.Mathematics;
 using UnityEditor.AdaptivePerformance.Editor;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class EnemyIA : MonoBehaviour
 {
 
     public int rutina;
@@ -18,42 +18,42 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Start()
     {
         ani = GetComponent<Animator>();
-        target = GameObject.Find("RobotKyle");
+        target = GameObject.Find("Player");
     }
 
 
-   
+
     public void Comportamiento_Enemigo()
-    { 
+    {
         if (Vector3.Distance(transform.position, target.transform.position) > 5)
         {
-          
-                ani.SetBool("run", false);
+
+            ani.SetBool("run", false);
             cronometro += 1 * Time.deltaTime;
-        if (cronometro >= 4)
-        {
-            rutina = UnityEngine.Random.Range(0, 2);
-            cronometro = 0;
-        }
-        switch (rutina)
-        {
-            case 0:
+            if (cronometro >= 4)
+            {
+                rutina = UnityEngine.Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
 
-                ani.SetBool("walk", false);
-                break;
+                    ani.SetBool("walk", false);
+                    break;
 
-            case 1:
-                grado = UnityEngine.Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
+                case 1:
+                    grado = UnityEngine.Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
 
-            case 2:
+                case 2:
 
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("walk", true);
-                break;
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
 
             }
 
@@ -62,17 +62,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, target.transform.position) > 1 && atacando)
             {
-        
-            
-            var lookPos = target.transform.position - transform.position;
-          
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
 
-            ani.SetBool("walk", false);
-            ani.SetBool("run", true);
-            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+                var lookPos = target.transform.position - transform.position;
+
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+
+                ani.SetBool("walk", false);
+                ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
 
 
                 ani.SetBool("attack", false);
@@ -82,18 +82,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
             else
             {
                 ani.SetBool("walk", false);
-                    ani.SetBool("run", false);
+                ani.SetBool("run", false);
 
-                     ani.SetBool ("attack", true);
-                     atacando = true;
+                ani.SetBool("attack", true);
+                atacando = true;
 
-                }
             }
-         }
+        }
+    }
     public void Final_Ani()
     {
         {
-   
+
             ani.SetBool("attack", false);
             atacando = false;
 
@@ -103,10 +103,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
 
         }
-   }
+    }
     void Update()
     {
-      Comportamiento_Enemigo();
+        Comportamiento_Enemigo();
     }
 }
 
